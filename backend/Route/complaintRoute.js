52f334d39963735b3df96c72d7f8069eafa58e52
Dhanushri-D <dhanushri.d2024cse@sece.ac.in>
2025-12-19 09:43:12 +0530

    Delete backend directory

--- backend/Route/complaintRoute.js
+++
@@ -1,7 +0,0 @@
-const express = require("express");
-const complaintController = require("./../Controller/complaintController");
-const complaintRoute = express.Router();
-complaintRoute.route("/").get(complaintController.getAllComplaints).post(complaintController.createComplaint);
-complaintRoute.route("/:id").get(complaintController.getComplaintById).put(complaintController.updateComplaint)
-.delete(complaintController.deleteComplaint);
-module.exports = complaintRoute;
